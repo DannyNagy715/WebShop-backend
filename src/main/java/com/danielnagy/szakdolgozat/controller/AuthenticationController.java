c3036012dfb06c38d9c4fcd22822dbcdfabd9c19
Daniel Nagy <danielnagy@daniels-mbp.t.hu>
2022-05-23 17:29:50 +0200

    cross origin added for front end

--- src/main/java/com/danielnagy/szakdolgozat/controller/AuthenticationController.java
+++ src/main/java/com/danielnagy/szakdolgozat/controller/AuthenticationController.java
@@ -1,4 +1,13 @@
 package com.danielnagy.szakdolgozat.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("api/auth")
 public class AuthenticationController {
+
+
 }
